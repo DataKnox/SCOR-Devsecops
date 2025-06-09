7266ece38f4bd452388d0af5ea271e9833b2a113
Knox Hutchinson <knox@knoxsdata.com>
2025-06-09 13:33:57 -0500

    add high risk operation

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import os
 def get_secret():
     password = "hardcoded_password"  # Bandit will flag this
     return password
+
+def dangerous_eval(user_input):
+    return eval(user_input)  # Bandit will flag this as HIGH
